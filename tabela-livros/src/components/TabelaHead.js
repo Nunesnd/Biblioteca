e99054582460499df9060de4fd668887a142fa3a
Diego Nunes <nunesdiegond@gmail.com>
2022-03-31 23:43:04 -0300

    adicinando componentes de tabela, adicionando arquivo json como base de dados, inserido arquivos de base e conteúdo no arquivo index.css

---
+++ tabela-livros/src/components/TabelaHead.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const TabelaHead = () => (
+    <tread>
+        <tr>
+            <th colSpan="4">Tabela de livros</th>
+        </tr>
+        <tr>
+            <th>ISBN</th>
+            <th>Título</th>
+            <th>Autor</th>
+            <th></th>
+        </tr>
+    </tread>
+);
+
+export default TabelaHead;
