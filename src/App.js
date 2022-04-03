b276ea14a31b6380090b7467f4eed11627caa8dc
Diego Nunes <nunesdiegond@gmail.com>
2022-04-03 11:21:17 -0300

    habilitado a remoção de linhas, adicionado setas para organizar titulos, mas tá bugado

--- src/App.js
+++ src/App.js
@@ -16,15 +16,38 @@ class App extends Component{
       console.log("Erro na requisição");
     })
     .finally(function(){
-      console.log("Sempre retorna");
+      console.log("Sempre retorna  Ihuuu");
     });
-  }
+  };
+
+  handleRemoveLinha = (id) => {
+    const livros = this.state.livros.filter(l => l.id !== id);
+    this.setState({livros});
+  };
+
+  handleOrdenarCrescente = titulo => {
+    const livros = this.state.livros.sort((a, b) => a.titulo < b.titulo ? -1 : 0);
+    this.setState({livros});
+  };
+
+  handleOrdenarDecrescente = titulo => {
+    const livros = this.state.livros.sort((a, b) => a.titulo < b.titulo ? -1 : 0); 
+    livros.reverse();
+    this.setState({livros});
+  };
+
 
   render() {
     return(
       <table className="tabela">
-        <TabelaHead/>
-        <TabelaBody livros={this.state.livros}/>
+        <TabelaHead
+          ordenarCrescente={this.handleOrdenarCrescente}
+          ordenarDecrescente={this.handleOrdenarDecrescente}        
+        />
+        <TabelaBody 
+          livros={this.state.livros}
+          removerLinha={this.handleRemoveLinha}
+        />
         <TabelaFoot qdeLivros={this.state.livros.length}/>
       </table>
     );
--- src/components/TabelaHead.js
+++ src/components/TabelaHead.js
@@ -1,14 +1,18 @@
 import React from 'react';
-const TabelaHead = () => (
+const TabelaHead = (props) => (
     <tread>
         <tr>
-            <th colSpan="4">Tabela de livros</th>
+            <th colSpan="3">Tabela de livros</th>
         </tr>
         <tr>
             <th>ISBN</th>
-            <th>Título</th>
+            <th>Título
+                <div className='container-setinhas'>
+                    <div onClick={() => props.ordenarCrescente()}>&#129093;</div> 
+                    <div onClick={() => props.ordenarDecrescente()}>&#129095;</div>
+                </div>
+            </th>
             <th>Autor</th>
-            <th></th>
         </tr>
     </tread>
 );
